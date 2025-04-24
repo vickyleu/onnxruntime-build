55c266497eaadf482e2d2e3fd909997240d0b16e
vickyleu <yu6564172@gmail.com>
2025-04-24 17:15:59 +0800

    Update toolchain for ARM and adjust ONNX Runtime version to 1.14.1

--- arm-linux-gnueabihf.toolchain.cmake
+++ arm-linux-gnueabihf.toolchain.cmake
@@ -5,11 +5,20 @@ set(CMAKE_CXX_STANDARD 17)            # 只填写整数
 set(CMAKE_CXX_STANDARD_REQUIRED ON)   # 如果编译器不支持则报错
 set(CMAKE_CXX_EXTENSIONS ON)         # GNU 扩展，是否使用纯标准模式
 
+
+
+# 设置GCC 9交叉编译器
 set(CMAKE_C_COMPILER "arm-linux-gnueabihf-gcc-9")
 set(CMAKE_CXX_COMPILER "arm-linux-gnueabihf-g++-9")
+
+# 设置sysroot路径
 # 下载的工具链路径sysroot 强制关闭c23特性
 # https://publishing-ie-linaro-org.s3.amazonaws.com/releases/components/toolchain/binaries/4.9-2017.01/arm-linux-gnueabihf/sysroot-eglibc-linaro-2017.01-arm-linux-gnueabihf.tar.xz
 set(CMAKE_SYSROOT "/home/vickyleu/build/arm-linux-gnueabihf/arm-linux-gnueabihf/sysroot")
+
+# 设置Iconv内置标志
+set(Iconv_IS_BUILT_IN ON)
+
 # 指定查找程序、库和包的路径前缀
 set(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT})
 # 调整查找策略
@@ -21,9 +30,15 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 set(CMAKE_C_SYSTEM_INCLUDE_PATH "${CMAKE_SYSROOT}/usr/include")
 set(CMAKE_CXX_SYSTEM_INCLUDE_PATH "${CMAKE_SYSROOT}/usr/include")
 
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -L${CMAKE_SYSROOT}/usr/lib")
-set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -L${CMAKE_SYSROOT}/usr/lib")
+# 确保链接使用sysroot中的库
+set(CMAKE_EXE_LINKER_FLAGS " -Wl,--sysroot=${CMAKE_SYSROOT} -L${CMAKE_SYSROOT}/usr/lib")
+set(CMAKE_SHARED_LINKER_FLAGS " -Wl,--sysroot=${CMAKE_SYSROOT} -L${CMAKE_SYSROOT}/usr/lib")
 
 # cache flags
-set(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -D_GNU_SOURCE -std=c11 -march=armv7-a -mfloat-abi=hard -mfpu=neon  -v" CACHE STRING "c flags")
-set(CMAKE_CXX_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -D_GNU_SOURCE -std=c++17 -D_GLIBCXX_USE_CXX11_ABI=1  -march=armv7-a -mfloat-abi=hard -mfpu=neon  -v" CACHE STRING "cxx flags")
+# 明确添加需要的头文件路径
+set(CMAKE_C_FLAGS_INIT "-nostdinc  -isystem ${CMAKE_SYSROOT}/usr/include \
+ -D_GNU_SOURCE -std=c11 -march=armv7-a -mfloat-abi=hard -mfpu=neon " CACHE STRING "c flags")
+set(CMAKE_CXX_FLAGS_INIT "-nostdinc -nostdinc++  -isystem ${CMAKE_SYSROOT}/usr/include \
+ -isystem ${CMAKE_SYSROOT}/usr/include/c++/4.9.4 \
+ -isystem ${CMAKE_SYSROOT}/usr/include/c++/4.9.4/arm-linux-gnueabihf \
+-D_GNU_SOURCE -std=c++17 -D_GLIBCXX_USE_CXX11_ABI=1  -march=armv7-a -mfloat-abi=hard -mfpu=neon " CACHE STRING "cxx flags")
