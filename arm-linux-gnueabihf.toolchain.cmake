2cd874de70587366b56b374ab8a4012d9cba83d4
vickyleu <yu6564172@gmail.com>
2025-04-24 17:05:02 +0800

    Update toolchain for ARM and adjust ONNX Runtime version to 1.14.1

--- arm-linux-gnueabihf.toolchain.cmake
+++ arm-linux-gnueabihf.toolchain.cmake
@@ -5,8 +5,8 @@ set(CMAKE_CXX_STANDARD 17)            # 只填写整数
 set(CMAKE_CXX_STANDARD_REQUIRED ON)   # 如果编译器不支持则报错
 set(CMAKE_CXX_EXTENSIONS ON)         # GNU 扩展，是否使用纯标准模式
 
-set(CMAKE_C_COMPILER "arm-linux-gnueabihf-gcc")
-set(CMAKE_CXX_COMPILER "arm-linux-gnueabihf-g++")
+set(CMAKE_C_COMPILER "arm-linux-gnueabihf-gcc-9")
+set(CMAKE_CXX_COMPILER "arm-linux-gnueabihf-g++-9")
 # 下载的工具链路径sysroot 强制关闭c23特性
 # https://publishing-ie-linaro-org.s3.amazonaws.com/releases/components/toolchain/binaries/4.9-2017.01/arm-linux-gnueabihf/sysroot-eglibc-linaro-2017.01-arm-linux-gnueabihf.tar.xz
 set(CMAKE_SYSROOT "/home/vickyleu/build/arm-linux-gnueabihf/arm-linux-gnueabihf/sysroot")
@@ -21,13 +21,9 @@ set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 set(CMAKE_C_SYSTEM_INCLUDE_PATH "${CMAKE_SYSROOT}/usr/include")
 set(CMAKE_CXX_SYSTEM_INCLUDE_PATH "${CMAKE_SYSROOT}/usr/include")
 
-set(CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES "")
-set(CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES "")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -L${CMAKE_SYSROOT}/usr/lib")
+set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -L${CMAKE_SYSROOT}/usr/lib")
 
-set(CMAKE_EXE_LINKER_FLAGS "-L${CMAKE_SYSROOT}/usr/lib -Wl,-rpath-link,${CMAKE_SYSROOT}/lib -Wl,-rpath-link,${CMAKE_SYSROOT}/usr/lib")
-set(CMAKE_SHARED_LINKER_FLAGS "-L${CMAKE_SYSROOT}/usr/lib -Wl,-rpath-link,${CMAKE_SYSROOT}/lib -Wl,-rpath-link,${CMAKE_SYSROOT}/usr/lib")
 # cache flags
-# 统一设置编译标志
-set(CMAKE_C_FLAGS_INIT "-D_GNU_SOURCE -std=c11 -march=armv7-a -mfloat-abi=hard -mfpu=neon -nostdinc -I${CMAKE_SYSROOT}/usr/include/c++/4.9.4/arm-linux-gnueabihf" CACHE STRING "c flags")
-set(CMAKE_CXX_FLAGS_INIT "-D_GNU_SOURCE -std=c++17 -D_GLIBCXX_USE_CXX11_ABI=1 -march=armv7-a -mfloat-abi=hard -mfpu=neon -nostdinc++ -I${CMAKE_SYSROOT}/usr/include/c++/4.9.4 -I${CMAKE_SYSROOT}/usr/include/c++/4.9.4/arm-linux-gnueabihf" CACHE STRING "cxx flags")
-
+set(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -D_GNU_SOURCE -std=c11 -march=armv7-a -mfloat-abi=hard -mfpu=neon  -v" CACHE STRING "c flags")
+set(CMAKE_CXX_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -D_GNU_SOURCE -std=c++17 -D_GLIBCXX_USE_CXX11_ABI=1  -march=armv7-a -mfloat-abi=hard -mfpu=neon  -v" CACHE STRING "cxx flags")
