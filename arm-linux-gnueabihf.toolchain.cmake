c9d4a0dad7dfef719c67f3041419ad9d36f16477
vickyleu <yu6564172@gmail.com>
2025-04-24 16:10:12 +0800

    Update toolchain for ARM and adjust ONNX Runtime version to 1.14.1

--- arm-linux-gnueabihf.toolchain.cmake
+++ arm-linux-gnueabihf.toolchain.cmake
@@ -10,16 +10,25 @@ set(CMAKE_CXX_COMPILER "arm-linux-gnueabihf-g++")
 # 下载的工具链路径sysroot 强制关闭c23特性
 # https://publishing-ie-linaro-org.s3.amazonaws.com/releases/components/toolchain/binaries/4.9-2017.01/arm-linux-gnueabihf/sysroot-eglibc-linaro-2017.01-arm-linux-gnueabihf.tar.xz
 set(CMAKE_SYSROOT "/home/vickyleu/build/arm-linux-gnueabihf/arm-linux-gnueabihf/sysroot")
-
+# 指定查找程序、库和包的路径前缀
+set(CMAKE_FIND_ROOT_PATH ${CMAKE_SYSROOT})
+# 调整查找策略
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
 set(CMAKE_C_SYSTEM_INCLUDE_PATH "${CMAKE_SYSROOT}/usr/include")
 set(CMAKE_CXX_SYSTEM_INCLUDE_PATH "${CMAKE_SYSROOT}/usr/include")
+
+set(CMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES "")
+set(CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES "")
 
 set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -L${CMAKE_SYSROOT}/usr/lib")
 set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -L${CMAKE_SYSROOT}/usr/lib")
 
 # cache flags
-set(CMAKE_C_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -D_GNU_SOURCE -std=c11 -march=armv7-a -mfloat-abi=hard -mfpu=neon  -v" CACHE STRING "c flags")
-set(CMAKE_CXX_FLAGS_INIT "${CMAKE_C_FLAGS_INIT} -D_GNU_SOURCE -std=c++17 -D_GLIBCXX_USE_CXX11_ABI=1  -march=armv7-a -mfloat-abi=hard -mfpu=neon  -v" CACHE STRING "cxx flags")
+# 统一设置编译标志
+set(CMAKE_C_FLAGS_INIT "-D_GNU_SOURCE -std=c11 -march=armv7-a -mfloat-abi=hard -mfpu=neon" CACHE STRING "c flags")
+set(CMAKE_CXX_FLAGS_INIT "-D_GNU_SOURCE -std=c++17 -D_GLIBCXX_USE_CXX11_ABI=1 -march=armv7-a -mfloat-abi=hard -mfpu=neon" CACHE STRING "cxx flags")
+
